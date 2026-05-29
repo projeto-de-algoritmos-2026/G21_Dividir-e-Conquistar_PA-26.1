811a84612bb31b9fd2a022eaefe64ebe921f8ddb
MIGUEL ARTHUR <miguell1040z@gmail.com>
2026-05-29 17:56:59 -0300

    feat: Commit 5 - Implementação da lógica Merge-and-Count

--- algorithms.js
+++ algorithms.js
@@ -15,8 +15,48 @@ const Algorithms = (() => {
         return inversions;
     };
 
+    /**
+     * Mescla duas metades ordenadas e conta as inversões cruzadas.
+     * @param {Array<number>} array - O vetor original
+     * @param {Array<number>} temp - Vetor temporário para o merge
+     * @param {number} left - Índice inicial da metade esquerda
+     * @param {number} mid - Índice final da metade esquerda
+     * @param {number} right - Índice final da metade direita
+     * @returns {number} - Contagem de inversões cruzadas
+     */
+    const mergeAndCount = (array, temp, left, mid, right) => {
+        let i = left;
+        let j = mid + 1;
+        let k = left;
+        let inversions = 0;
+
+        while ((i <= mid) && (j <= right)) {
+            if (array[i] <= array[j]) {
+                temp[k++] = array[i++];
+            } else {
+                temp[k++] = array[j++];
+                inversions += (mid - i + 1);
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = array[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = array[j++];
+        }
+
+        for (i = left; i <= right; i++) {
+            array[i] = temp[i];
+        }
+
+        return inversions;
+    };
+
     return {
-        countInversionsBruteForce
+        countInversionsBruteForce,
+        mergeAndCount
     };
 })();
 
